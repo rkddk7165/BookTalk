841eae86ab96b114d61b14415c12826dcf238a88
rkddk7165 <rkddk7165@naver.com>
2025-07-31 19:21:34 +0900

    시작

---
+++ booktalk/booktalk/src/main/java/myproject/booktalk/BooktalkApplication.java
@@ -0,0 +1,13 @@
+package myproject.booktalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktalkApplication.class, args);
+	}
+
+}
---
+++ booktalk/booktalk/src/test/java/myproject/booktalk/BooktalkApplicationTests.java
@@ -0,0 +1,13 @@
+package myproject.booktalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
